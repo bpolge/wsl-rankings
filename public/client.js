ef8e3a8f79b36c32fe066bc2ca3e3d3fde4dc273
Benjamin Polge <ben.polge@twintechs.com>
2017-03-17 20:51:53 -0500

    Changed the style on the athlete list

--- public/client.js
+++ public/client.js
@@ -1,9 +1,19 @@
 $(() => {
 
+  // Need to add handlebars here at some point
   function append(elems) {
-    let list = [];
+    const list = [];
     for (var i in elems) {
-      list.push($('<li>', { text: elems[i] }));
+      const item = elems[i];
+      const content =
+  `<div class="athlete-tile">
+    <a class="headshot" href="/athletes/${item.name}"><img class="athlete-img" src="${item.imgSrc}" alt="${item.name}"/></a>
+    <span class="text-wrap">
+      <a href="/athletes/${item.name}">${item.name}</a>
+      <span>${item.rank}</span>
+    </span>
+  </div>`;
+      list.push($('<li>', { html: content }));
     }
     $('.rankings').append(list);
   }
