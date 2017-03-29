00a63f2ad73991468b1f0fa0563b9b2b165d3256
Benjamin Polge <ben.polge@twintechs.com>
2017-03-29 16:46:40 -0500

    forgot to add back changes to container.jsx

--- frontend/js/container.jsx
+++ frontend/js/container.jsx
@@ -1,8 +1,32 @@
+import React from 'react';
 import { connect } from 'react-redux';
-import { AthleteList } from './athlete.jsx';
+import { Athlete, AthleteList } from './athlete.jsx';
+import { toggleSelected } from './actions';
 
-const mapStateToProps = state => ({
+const AthletesContainer = ({ dispatch, athletes, selectedAthlete }) => {
+  function classForItem(item) {
+    let className = 'athlete-list-item';
+    className += item.id === selectedAthlete ? ' selected' : '';
+    return className;
+  }
+
+  return (
+    <AthleteList>
+      {athletes.map(item =>
+        <li
+          key={item.id}
+          className={classForItem(item)}
+          onMouseUp={() => dispatch(toggleSelected(item.id))}
+        >
+          <Athlete athlete={item} />
+        </li>
+      )}
+    </AthleteList>
+  );
+};
+
+const mapListProps = state => ({
   athletes: state.athletes,
+  selectedAthlete: state.selectedAthlete,
 });
-
-export default connect(mapStateToProps)(AthleteList);
+export default connect(mapListProps)(AthletesContainer);
